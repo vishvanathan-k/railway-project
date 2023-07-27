58e7da644d4dedc4018d5bc5ef13e182748fa4dd
Vishvanathan K <vishvanathank@outlook.in>
2023-07-27 16:18:50 +0530

    Add NavBar and Update icons

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,4 +1,5 @@
 import { Head } from "$fresh/runtime.ts";
+import Navbar from "../components/NavBar.tsx";
 import { AppProps } from "$fresh/src/server/types.ts";
 
 export default function App(props: AppProps) {
@@ -9,10 +10,11 @@ export default function App(props: AppProps) {
       <Head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <link rel="icon" type="image/png" href="../favicon.png"/>
+        <link rel="icon" type="image/ico" href="../favicon.ico"/>
         <link rel="stylesheet" href="../styles/tailwind.css" />
       </Head>
       <body>
+        <Navbar/>
         <Component />
       </body>
     </html>
